d1c53a19c30cf1b11c21fcc0d59d46444ff09e64
Richard Jimenez <richardjimenez176@gmail.com>
2021-02-28 13:38:03 -0400

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class FileController extends Controller
+{
+    public function store(Request $request, string $fileName)
+    {
+
+        $content = $request->getContent();
+
+        $token = Str::random();
+
+        Storage::disk('local')->put("public/$token/$fileName", $content);
+
+        return url('/') . "/$token/" . $fileName;
+    }
+
+    public function get(Request $request, $token, $fileName)
+    {
+        return Storage::disk('local')->get("public/$token/$fileName");
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\UploadController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::put('/{fileName}', [FileController::class, 'store']);
+Route::get('/{token}/{fileName}', [FileController::class, 'get']);
